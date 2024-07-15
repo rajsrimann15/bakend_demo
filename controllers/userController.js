85bab65fc3596c63645b965941cc81ee5bfb8adb
rajsrimann15 <rajsrimannwork@gmail.com>
2024-07-15 21:43:23 +0530

    k

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/authModel");
 
